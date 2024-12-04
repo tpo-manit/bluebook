d59871e67428333e56eb18c6496e052e9facf037
theSoberSobber <akshatshrivastava0010@gmail.com>
2024-12-04 17:23:54 +0530

    [DBG] Stupid LLM

--- responses/scripts/buildPrompt.py
+++ responses/scripts/buildPrompt.py
@@ -29,7 +29,7 @@ if not pr_data["email"].endswith(".ac.in"):
 else:
     qa_data = pr_data["data"]
 
-    prompt = "You are given a bunch of questions and answers pertaining to a document that contains interview transcripts for candidates that have been selected into companies, please flag the document into suitable for publishing on a public facing site or not. Provide a one liner reason flagging the parts that are problematic. We are defining problematic here as profanities/slurs etc. The text can be flagged if the content doesn't seem relevant to our interview experience publishing site too (basically we are trying to avoid spam and hateful speech). The text can be multilingual. You must output in this format: <Yes/No>. <Reasoning Highlighting the Problematic Parts if No>.\n\n"
+    prompt = "You are given a bunch of questions and answers pertaining to a document that contains interview transcripts for candidates that have been selected into companies, please flag the document into suitable for publishing on a public facing site or not. Provide a one liner reason flagging the parts that are problematic. We are defining problematic here as profanities/slurs etc. The text can be flagged if the content doesn't seem relevant to our interview experience publishing site too (basically we are trying to avoid spam and hateful speech). The text can be multilingual. You must output in this format: <Yes/No>. <Reasoning Highlighting the Problematic Parts if No>. Please note that we are not expected an evaluation for each question, just one singular evaluation for the whole thing in the given input format.\n\n"
 
     for question, answer in qa_data.items():
         if not len(answer): continue

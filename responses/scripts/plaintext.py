7f9940964d495e29444c8433c712c962e77a8098
theSoberSobber <akshatshrivastava0010@gmail.com>
2024-12-04 15:41:18 +0530

    Try?

--- responses/scripts/plaintext.py
+++ responses/scripts/plaintext.py
@@ -4,8 +4,8 @@ import os
 file_path = os.getenv('GITHUB_EVENT_PATH')
 with open(file_path, 'r') as f:
     pr_data = json.load(f)
-
-qa_data = pr_data['data']
+print(pr_data)
+qa_data = pr_data["data"]
 questions = list(qa_data.keys())
 answers = list(qa_data.values())
 

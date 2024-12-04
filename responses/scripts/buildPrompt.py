dbbd05b0a65c76e59fd8c13aeb44bb9ea27fe928
theSoberSobber <akshatshrivastava0010@gmail.com>
2024-12-04 16:41:18 +0530

    Try again again again again?

--- responses/scripts/buildPrompt.py
+++ responses/scripts/buildPrompt.py
@@ -23,7 +23,6 @@ file_path = get_latest_response_file(responses_folder=responses_folder)
 with open(file_path, 'r') as f:
     pr_data = json.load(f)
 
-print(pr_data)
 if not pr_data["email"].endswith(".ac.in"):
     moderation_output = "No, Unauthorized Email."
     with open('/tmp/moderation_result.txt', 'w') as result_file:
@@ -31,14 +30,10 @@ if not pr_data["email"].endswith(".ac.in"):
 else:
     qa_data = pr_data["data"]
 
-    prompt = "You are given a bunch of questions and answers pertaining to a document that contains interview transcripts for candidates that have been selected into companies, please flag the document into suitable for publishing on a public facing site or not. Provide a one liner reason flagging the parts that are problematic. We are defining problematic here as profanities/slurs etc. The text can be flagged if the content doesn't seem relevant to our interview experience publishing site too (basically we are trying to avoid spam and hateful speech). The text can be multilingual. You must output in this format: <Yes/No>. <Reasoning Highlighting the Problematic Parts>.\n\n"
+    prompt = "You are given a bunch of questions and answers pertaining to a document that contains interview transcripts for candidates that have been selected into companies, please flag the document into suitable for publishing on a public facing site or not. Provide a one liner reason flagging the parts that are problematic. We are defining problematic here as profanities/slurs etc. The text can be flagged if the content doesn't seem relevant to our interview experience publishing site too (basically we are trying to avoid spam and hateful speech). The text can be multilingual. You must output in this format: <Yes/No>. <Reasoning Highlighting the Problematic Parts if No>.\n\n"
 
     for question, answer in qa_data.items():
         if not len(answer): continue
         prompt += f"Question: {question}\nAnswer: {', '.join(answer)}\n\n"
 
-response = ollama.chat(model="mistral", messages=[{"role": "user", "content": prompt}])
-moderation_output = response['text']
-with open('/tmp/moderation_result.txt', 'w') as result_file:
-    result_file.write(moderation_output)
-print(moderation_output)
+print(prompt)
